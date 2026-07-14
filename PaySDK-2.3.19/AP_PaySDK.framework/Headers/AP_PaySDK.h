74aac63e23b4a7853bbb8ce363feb66f8d93fc0b
priyanka-asiapay <priyanka.g@asiapay.com>
2026-07-14 17:17:53 +0530

    alipaysdk-15.8.42.260429154144 for Stable AlipayHK APP invocation,Seamless native payment experience and Reduced H5 fallback rate also improved conversion rate

---
+++ PaySDK-2.3.19/AP_PaySDK.framework/Headers/AP_PaySDK.h
@@ -0,0 +1,30 @@
+//
+//  AP_PaySDK.h
+//  AP_PaySDK
+//
+//  Created by Priyanka on 28/11/19.
+//  Copyright © 2019 Asiapay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "WebViewJavascriptBridge.h"
+#import "Wrapper.h"
+#import <CommonCrypto/CommonHMAC.h>
+#import "UPPaymentControl.h"
+#import "WXApi.h"
+#import "WXApiObject.h"
+#import "WKWebViewJavascriptBridge.h"
+#import "WebViewJavascriptBridge_JS.h"
+#import "WechatAuthSDK.h"
+
+
+
+//! Project version number for AP_PaySDK.
+FOUNDATION_EXPORT double AP_PaySDKVersionNumber;
+
+//! Project version string for AP_PaySDK.
+FOUNDATION_EXPORT const unsigned char AP_PaySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AP_PaySDK/PublicHeader.h>
+
+
